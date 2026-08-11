e8717e25c5698b640aca5cedf0964d5f5913adf5
senacchamadas-beep <senacchamadas@gmail.com>
2026-08-11 03:17:11 -0300

    mdc pi2

--- firebase-config.js
+++ firebase-config.js
@@ -3,6 +3,12 @@
 // Estas chaves são públicas por design — proteja com Regras do Firestore.
 // Configurações do projeto Firebase Real
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyBeGZ6kXjS21k2WVHlkzDNnsStVT9IiSuU",
   authDomain: "barbearias-40070.firebaseapp.com",
@@ -10,5 +16,8 @@ const firebaseConfig = {
   projectId: "barbearias-40070",
   storageBucket: "barbearias-40070.firebasestorage.app",
   messagingSenderId: "784765952485",
-  appId: "1:784765952485:web:b803a25f484b6a53554f0c"
+  appId: "1:784765952485:web:eed1709e4e7d3ab1554f0c"
 };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
