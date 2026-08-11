a3f9d866a65903d9486eec1cdeda43cfd24a87bf
senacchamadas-beep <senacchamadas@gmail.com>
2026-08-10 23:38:35 -0300

    mdc pi

--- firebase-config.js
+++ firebase-config.js
@@ -2,12 +2,12 @@
 // Console: https://console.firebase.google.com > Configurações do projeto > Seus apps (Web)
 // Estas chaves são públicas por design — proteja com Regras do Firestore.
 // Configurações do projeto Firebase Real
-export const firebaseConfig = {
-  apiKey: "AIzaSyBeGZ6kXJS21kZMVHlkzDNnsStVT9Ti5uU",
-  authDomain: "://firebaseapp.com",
-  databaseURL: "https://firebaseio.com",
+const firebaseConfig = {
+  apiKey: "AIzaSyBeGZ6kXjS21k2WVHlkzDNnsStVT9IiSuU",
+  authDomain: "barbearias-40070.firebaseapp.com",
+  databaseURL: "https://barbearias-40070-default-rtdb.firebaseio.com",
   projectId: "barbearias-40070",
   storageBucket: "barbearias-40070.firebasestorage.app",
-  messagingSenderId: "7704265952405",
-  appId: "1:7704265952405:web:b803a25f484b6a53564f0c"
+  messagingSenderId: "784765952485",
+  appId: "1:784765952485:web:b803a25f484b6a53554f0c"
 };
